a9dec7ed0bf169227cdb7e71e1cce1ce0bf3e9c3
unknown <asifkeloth@gmail.com>
2026-07-06 13:54:41 +0530

    fear:java edited

--- java/java.js
+++ java/java.js
@@ -1,6 +1,20 @@
-const button= document.getElementById("menu-btn")
-button.addEventListener("click",()=>{
-    const divremove = document.getElementsByClassName("remove")
-    divremove.style = "display:visible"
+const button = document.getElementById("menu")
+const divremove = document.getElementById("remove")
+const remove = document.getElementById("remove-btn")
+
+
+console.log(button);
+
+button.addEventListener("click", () => {
+    // console.log(show);
+
+
+    divremove.style = "translate:0;  transition-duration:800ms"
+
+})
+remove.addEventListener("click", () => {
+    console.log("click");
+
+    divremove.style ="translate:-100; transition-duration:450ms "
 
 })
